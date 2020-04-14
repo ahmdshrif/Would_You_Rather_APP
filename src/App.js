09672409732d3d526a699890b0526282279feb1d
ahmed ashraf <a.s1171111@gmail.com>
2020-04-14 14:27:23 +0200

    UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import LoadingBar from 'react-redux-loading'
+import { connect } from 'react-redux'
+import Header from './components/common/Header'
+import 'react-loading-bar/dist/index.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/pages/allqustions'
+import LeaderBoard from './components/pages/LeaderBoard'
+import NewQustion from './components/pages/NewQustion'
+import SignIn from './components/pages/SignIn'
+class App extends React.Component {
+  state = {
+    show: false
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <div className='container'>
+            <Header />
+            {this.props.loading === true
+              ? null
+              : <div>
+                <Route path='/' exact component={Home} />
+                <Route path='/newqustion' exact component={NewQustion} />
+                <Route path='/leaderBoard' exact component={LeaderBoard} />
+                <Route path='/signIn' exact component={SignIn} />
+              </div>}
+          </div>
+        </div>
+      </BrowserRouter>
+
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
